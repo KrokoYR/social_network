9c7a5c6f00604d78cc7d5a9ee6e38400cbc6f785
Lopson <balzhinimaev-2011@mail.ru>
2020-04-22 20:50:32 +0900

    Added react-router-dom(+@type) for routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,27 @@ import './App.css';
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
+import Dialogs from "./components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from 'react-router-dom';
+
 
 function App() {
-  return (
-    <div className="app-wrapper">
-      <Header />
-      <Navbar />
-      <Profile />
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<div className="app-wrapper">
+				<Header/>
+				<Navbar/>
+				<div className={'app-wrapper-content'}>
+					<Route path={'/profile'} component={Profile}/>
+					<Route path={'/dialogs'} component={Dialogs}/>
+					<Route/>
+					<Route/>
+					<Route/>
+				</div>
+				{/*<Profile/>*/}
+			</div>
+		</BrowserRouter>
+	);
 }
 
 export default App;
