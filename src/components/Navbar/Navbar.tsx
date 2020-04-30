88dfa77d3d3eac0d2711f3cc6c7a16f633bb562e
Lopson <balzhinimaev-2011@mail.ru>
2020-04-30 18:23:32 +0900

    Recreate Navbar and its structure

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,31 +1,39 @@
 import React from 'react';
 import styles from './Navbar.module.css';
-import { NavLink } from 'react-router-dom';
+import {NavLink} from 'react-router-dom';
 import Friends from "./Friends/Friends";
+import ProfileLink from "./ProfileLink/ProfileLink";
 
 
 const Navbar = () => {
-  return (
-    <nav className={styles.navbar}>
-      <ul className={styles.navbar__wrapper}>
-         <li>
-            <NavLink className={styles.navbar__link} activeClassName={styles.active} to="/profile">Profile</NavLink>
-         </li>
-         <li>
-            <NavLink className={styles.navbar__link} activeClassName={styles.active} to="/dialogs">Messages</NavLink>
-         </li>
-         <li>
-            <NavLink className={styles.navbar__link} activeClassName={styles.active} to="/news">News</NavLink>
-         </li>
-         <li>
-            <NavLink className={styles.navbar__link} activeClassName={styles.active} to="/music">Music</NavLink>
-         </li>
-         <li>
-            <NavLink className={styles.navbar__link} activeClassName={styles.active} to="/settings">Setting</NavLink>
-         </li>
-      </ul>
-        <Friends/>
-    </nav>
-  )}
+	return (
+		<nav className={styles.navbar}>
+			<ProfileLink></ProfileLink>
+			<ul className={styles.navbar__wrapper}>
+				<li>
+					<NavLink className={styles.navbar__link} activeClassName={styles.active} to="/news">
+						<span className={'fa fa-newspaper-o'}/>News
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={styles.navbar__link} activeClassName={styles.active} to="/dialogs">
+						<span className={'fa fa-comments'}/>Messenger
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={styles.navbar__link} activeClassName={styles.active} to="/music">
+						<span className={'fa fa-music'}/>Music
+					</NavLink>
+				</li>
+				<li>
+					<NavLink className={styles.navbar__link} activeClassName={styles.active} to="/settings">
+						<span className={'fa fa-video-camera'}/>Setting
+					</NavLink>
+				</li>
+			</ul>
+			
+		</nav>
+	)
+}
 
 export default Navbar;
