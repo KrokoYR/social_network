76f3cba0ff095e0f945650badd4408095d54ac7b
Lopson <balzhinimaev-2011@mail.ru>
2020-04-30 18:21:41 +0900

    Update index and app structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,59 @@
-import React, {FC} from 'react';
+import React from 'react';
 import './App.css';
+
+import {connect} from "react-redux";
+import { BrowserRouter, Route, Redirect } from 'react-router-dom';
+
+// Types:
+
+
+// Components:
 import Header from './components/Header/Header'
 import Navbar from './components/Navbar/Navbar'
 import Profile from './components/Profile/Profile'
-import Dialogs from "./components/Dialogs/Dialogs";
-import { BrowserRouter, Route } from 'react-router-dom';
-import { MESSAGES_OBJ } from "./store/Reducers/Dialogs/Message/types";
-import { RECIPIENT_OBJ } from "./store/Reducers/Dialogs/Recipient/types";
-import { POST_OBJ } from './store/Reducers/Profile/PostsWall/Post/types';
+
+// Containers:
+import DialogsContainer from "./components/Dialogs/Dialogs-container";
+
+// States:
+import { AppState } from "./store";
+import { DialogsState } from './store/Reducers/Dialogs/types';
+import News from "./components/News/News";
 
 export interface AppProps {
-	state: {
-		recipients: RECIPIENT_OBJ[],
-		messages: MESSAGES_OBJ[],
-		posts: POST_OBJ[],
-		addPost: (newPost:string) => void
+	dialogs: DialogsState
+}
+
+class App extends React.Component<AppProps> {
+	
+	state = {}
+	
+	render() {
+		return (
+			<BrowserRouter>
+				<div className="app-wrapper">
+					<Header/>
+					<Navbar/>
+					<div className={'app-wrapper-content'}>
+						<Route path={'/'} render={() => <Redirect to={'/news'}/>}/>
+						<Route path={'/profile'} render={() =><Profile posts={[]}/>}/>
+						<Route path={'/dialogs'} render={() =><DialogsContainer />}/>
+						<Route path={'/news'} render={() => <News/>}/>
+						<Route/>
+						<Route/>
+					</div>
+				</div>
+			</BrowserRouter>
+		);
 	}
 }
 
-const App = () => {
-	return (
-		<BrowserRouter>
-			<div className="app-wrapper">
-				<Header/>
-				<Navbar/>
-				<div className={'app-wrapper-content'}>
-					{/*<Route path={'/profile'} component={Profile}/>*/}
-					{/*<Route path={'/dialogs'} component={Dialogs}/>*/}
-					<Route path={'/profile'} render={
-						(profileData) =>
-							<Profile posts={[]}/>}/>
-					
-					<Route path={'/dialogs'} render={
-						(dialogsData) =>
-							<Dialogs recipientData={[]}
-							         messagesData={[]}/>
-						}/>
-					<Route/>
-					<Route/>
-					<Route/>
-				</div>
-				{/*<Profile/>*/}
-			</div>
-		</BrowserRouter>
-	);
+const mapStateToProps = (state: AppState) => {
+	return {
+		dialogs: state.dialogs
+	}
 }
 
-export default App;
+export default connect(
+	mapStateToProps
+)(App);
